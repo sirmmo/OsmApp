b4808d69db72b1dd44681d09de2e2942baf33ac8
Marco Montanari <sirmmo@gmail.com>
2014-07-18 16:33:51 +0200

    first apis

--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	pass
+
+
+
+
+#API
+def api(request):
+	app = request.REQUEST.get("app")
+	client = request.REQUEST.get("client")
+	operation = request.REQUEST.get("op")
+	detail = request.REQUEST.get("mode")
+
+	pass
+
+
+#INNER API FOR CODE GENERATION
+def generator(request):
+	app = request.REQUEST.get("app")
+	os = request.REQUEST.get("os")
+	namespace = request.REQUEST.get("ns")
+
+	#os defines template
+
+	#app defines strings
+
+	#namespace defines dir structure and strings.
+
+	
