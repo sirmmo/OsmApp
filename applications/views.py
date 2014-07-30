f679c6ce4393c5833f5ceae2ffa01de2c63fc8b5
Marco Montanari <sirmmo@gmail.com>
2014-07-30 02:28:49 +0200

    stuff

--- applications/views.py
+++ applications/views.py
@@ -1,20 +1,72 @@
 from django.shortcuts import render
 
+from osmapi import OsmApi
+
+from django.conf import settings
+
 # Create your views here.
 
 def index(request):
-	pass
+	return render(request, "index.html")
 
-
+def app_page(rquest, app_id):
+	return render(request, "app.html")
 
 
 #API
+
+def app_stats(request, app_id):
+	pass
+
+def login(request):
+	userid = request.REQUEST.get("user")
+	password = request.REQUEST.get("password")
+	pass
+
 def api(request):
 	app = request.REQUEST.get("app")
 	client = request.REQUEST.get("client")
 	operation = request.REQUEST.get("op")
-	detail = request.REQUEST.get("mode")
+	mode = request.REQUEST.get("mode")
+	data = request.REQUEST.get("data", {"none":None})
+	if not all([app,client,operation, mode, data]):
+		return 
 
+	MyApi = OsmApi.OsmApi(username = settings.get("osm_user"), password = settings.get("osm_pass"))
+	
+	if operation == "write":
+		MyApi.ChangesetCreate({u"comment": u"OsmApp Upload"})
+
+
+
+		MyApi.ChangesetClose()
+
+	else:
+		OVERPASS_STRING="""
+			<osm-script output="json" timeout="25">
+			  <!-- gather results -->
+			  <union>
+			    <!-- query part for: "vending=condoms" -->
+			    <query type="node">
+			      <has-kv k="vending" v="condoms"/>
+			      <bbox-query {{bbox}}/>
+			    </query>
+			    <query type="way">
+			      <has-kv k="vending" v="condoms"/>
+			      <bbox-query {{bbox}}/>
+			    </query>
+			    <query type="relation">
+			      <has-kv k="vending" v="condoms"/>
+			      <bbox-query {{bbox}}/>
+			    </query>
+			  </union>
+			  <!-- print results -->
+			  <print mode="body"/>
+			  <recurse type="down"/>
+			  <print mode="skeleton" order="quadtile"/>
+			</osm-script>
+		"""
+	
 	pass
 
 
@@ -30,4 +82,4 @@ def generator(request):
 
 	#namespace defines dir structure and strings.
 
-	
+
--- osmapp/urls.py
+++ osmapp/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'osmapp.views.home', name='home'),
+    url(r'^$', 'applications.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
